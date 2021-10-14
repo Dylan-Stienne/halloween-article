f70ed607d1dd477d86dcca79c6dd92159e45484d
Benjamin B <Benjamin.burstein@edu.devinci.fr>
2021-10-14 09:42:36 +0200

    Import skull glb modele

--- website/components/Canvas3D.vue
+++ website/components/Canvas3D.vue
@@ -1,13 +1,12 @@
 <template>
-  <div id="container">
-    <nav>Canva</nav>
-    <div id="object"></div>
-  </div>
+<section>
+   <div id="object"></div>
+</section>
 </template>
 <script>
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 export default {
   name: 'Canvas3d',
   data() {
@@ -16,7 +15,7 @@ export default {
       renderer: null,
       scene: null,
       camera: null,
-      controls: null
+      controls: null,
     }
   },
   mounted() {
@@ -24,24 +23,39 @@ export default {
     this.animate()
   },
   methods: {
-    init() {
+    init () {
       this.scene = new THREE.Scene();
       this.camera = new THREE.PerspectiveCamera(
-        75,
+        16,
         window.innerWidth / window.innerHeight,
         0.1,
         1000
       );
 
       this.renderer = new THREE.WebGLRenderer({alpha: true});
-      this.renderer.setSize(window.innerWidth, window.innerHeight);
-      document.getElementById('object').appendChild(this.renderer.domElement);
+      this.renderer.setSize( window.innerWidth/4 , window.innerHeight/4);
+      this.renderer.domElement.style.margin = "auto";
+      document.getElementById('object').appendChild(this.renderer.domElement)
 
-      const geometry = new THREE.BoxGeometry(1, 1, 1);
 
-      const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-      this.cube = new THREE.Mesh(geometry, material);
-      this.scene.add(this.cube);
+      // const geometry = new THREE.BoxGeometry(1, 1, 1);
+
+      // const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+      // this.cube = new THREE.Mesh(geometry, material);
+       const loader = new GLTFLoader();
+      const url = '/models/skull.glb';
+       loader.loadAsync(url, ( gltf ) =>
+        {
+           const skull = gltf.scene;  // skull 3D object is loaded
+           console.log(skull)
+           skull.scale.set(2, 2, 2);
+           skull.position.y = 4;
+           this.scene.add(skull);
+        }, undefined, ( error ) => {
+
+	        console.error( error );
+
+        });
 
       this.controls = new OrbitControls(this.camera, this.renderer.domElement)
 
